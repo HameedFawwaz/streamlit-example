eb78adfe57f5a4b4eb80ec4ce3f8fbd1c0c11012
Fawwaz Hameed <55466919+HameedFawwaz@users.noreply.github.com>
2023-12-30 18:42:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,43 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from pathlib import Path as path
+from moviepy.editor import VideoFileClip
+from processes.process import process
+import shutil
+import os
 
-"""
-# Welcome to Streamlit!
+st.title("Deep Fake Generator!!!!!")
+st.header("yipee!!!")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+with st.form(key="Form :", clear_on_submit= True):
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    img = st.file_uploader(label="upload the image you want to deepfake")
+    temp = st.file_uploader(label="upload the template you would like to use")
+    submit = st.form_submit_button(label="submit")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if submit:
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    st.write("Processing... Please allow up to 15 minutes for your request to process.")
+    img_path = path("./saved", img.name)
+    temp_path = path("./saved", temp.name)
+    with open(img_path, mode="wb") as w:
+        w.write(img.getvalue())
+    with open(temp_path, mode="wb") as w:
+        w.write(temp.getvalue())
+        
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    if img_path.exists() and temp_path.exists():  
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+        len_vid = VideoFileClip(f"./saved/{temp.name}").duration
+        st.write("video length: " + str(len_vid))
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+        if len_vid <= 30:
+            process.generate(img.name, temp.name)
+
+            img_name = img.name.partition(".")
+            video_file = open(f'./generated/{img_name}.mp4', "rb")
+            video_bytes = video_file.read()
+
+            st.video(video_bytes)
+        else:
+            st.write("Error! Make sure that your template video is less than 30 seconds in length!")
+
